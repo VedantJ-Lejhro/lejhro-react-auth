7c360e5b225b3919d0f5b7fdd252d3a545f95667
VedantJ-Lejhro <vedant.jaiswal@lejhro.com>
2021-11-23 18:51:33 +0530

    repo initialised

---
+++ src/components/login.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react/cjs/react.production.min"
+
+const login = () => {
+
+    return (
+        <Fragment>
+            <div>
+                <input type="email" placeholder="E-mail" />
+            </div>
+        </Fragment>
+    )
+}
+export default login
